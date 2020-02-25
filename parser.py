4ea30a1c2086d42cd55b86dc7e5654a02746a79e
noobish.in@gmail.com <noobish.in@gmail.com>
2020-02-25 15:41:28 +0900

    modify req url

--- parser.py
+++ parser.py
@@ -7,7 +7,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 ## HTTP GET Request
-req = requests.get('https://www.ssgdfm.com/shop/dqDisplay/productCategoryMain?dispCtgrId=00010101&cateDepth=3&dispCtgrName=%EC%8A%A4%ED%82%A8%C2%B7%ED%86%A0%EB%84%88')
+req = requests.get('http://kor.lottedfs.com/kr/display/category/first?dispShopNo1=1100001&treDpth=1')
 
 
 ## HTML 소스 가져오기
@@ -20,7 +20,7 @@ soup = BeautifulSoup(html, 'html.parser')
 
 ## css selector로 html 요소 찾기
 my_titles = soup.select(
-    '#productResultArea > li > div > a'
+    '#prdList > div.imgType > ul > li > a'
 )
 
 ## my_titles는 bs의 list 객체
@@ -30,10 +30,10 @@ for title in my_titles:
     ##태그 안에 텍스트 가져오기
     #print(title.get('title'))
     ## tag의 속성을 가져오기(ex: href속성)
-    price_usd = title.find_all('span', class_='us-currency')
+    price_usd = title.find_all('div', class_='discount')
     price_krw = title.find_all('span', class_='nation-currency')
     #print(price_usd[0].text + price_krw[0].text)
-    data[title.get('title')] = price_usd[0].text + price_krw[0].text
+    data[title.get('data-gaevtlabel')] = price_usd[0].text + price_krw[0].text
 
 with open(os.path.join(BASE_DIR, 'result.json'), 'w+') as json_file:
     json.dump(data, json_file)
